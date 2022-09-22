cac64e1a4e2cefafff0a12db82850caa8f79ab46
Dhanashri Jagadale <93074307+dhanashrijagadale@users.noreply.github.com>
2022-09-22 22:16:37 +0100

    Add files via upload

---
+++ preprocessor.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def preprocess(df,region_df):
+	# filtering only summer Olympics
+	df = df[df['Season'] == 'Summer']
+	# merge with region_df
+	df = df.merge(region_df , on = 'NOC' ,how = 'left')
+	# dropping duplicates
+	df.drop_duplicates(inplace = True)
+	# One Hot Encoding Medals
+	df = pd.concat([df , pd.get_dummies(df['Medal'])], axis=1)
+	return df
